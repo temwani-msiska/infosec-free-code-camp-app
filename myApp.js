9d47625604bd3b17926dde607c1b817b062186ad
temwani-msiska <138024035+temwani-msiska@users.noreply.github.com>
2023-08-09 15:02:51 +0200

    Add intial application

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+const PORT = process.env.PORT || 3030;
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
 
 
 
