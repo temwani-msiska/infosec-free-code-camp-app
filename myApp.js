38cc5bf30fe30d2ad4887440f520a71ea6d81d14
temwani-msiska <138024035+temwani-msiska@users.noreply.github.com>
2023-08-09 19:33:49 +0200

    lets see

--- myApp.js
+++ myApp.js
@@ -2,55 +2,15 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 const PORT = process.env.PORT || 3030;
-// your code
 
-app.use (helmet.hidePoweredBy());
+// Use helmet.middlewares
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: 'deny' }));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Your other code...
 
 module.exports = app;
+
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
@@ -58,6 +18,7 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
